5368548a3b36444027ae0ed8d188ba74902b6ac0
Rohit Girdhar <rohitgird12@gmail.com>
2017-06-05 04:43:47 +0530

    k

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+app.set('port', (process.env.PORT || 5000));
+var server = app.listen(app.get('port'), function(){
+    console.log('listening for requests on port 5000,');
 });
 
 // Static files
@@ -23,3 +24,4 @@ io.on('connection', (socket) => {
     });
 
 });
+
